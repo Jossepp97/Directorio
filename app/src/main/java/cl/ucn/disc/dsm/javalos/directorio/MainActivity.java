e75476a61f24ce141caf5d9fb15819f2b9454e2a
José Ávalos <josseavalos97@gmail.com>
2021-11-18 04:36:06 -0300

    fix[Main]: Add Comments

--- app/src/main/java/cl/ucn/disc/dsm/javalos/directorio/MainActivity.java
+++ app/src/main/java/cl/ucn/disc/dsm/javalos/directorio/MainActivity.java
@@ -3,11 +3,19 @@ package cl.ucn.disc.dsm.javalos.directorio;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
+/**
+ * @author Jose Avalos Guzman
+ */
 public class MainActivity extends AppCompatActivity {
 
+    /**
+     *
+     * @param savedInstanceState the stace.
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
